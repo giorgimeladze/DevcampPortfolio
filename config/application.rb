e7217c2013196aaa96563173868165f16e1c9ba5
Giorgi Meladze <giorgi.meladze711@gmail.com>
2020-02-17 22:14:13 +0400

    added ENV variables

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ENV.update YAML.load(File.read(File.expand_path('../local_env.yml', __FILE__)))
+
 module DevcampPortfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
