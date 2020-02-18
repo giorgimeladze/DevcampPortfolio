b652c783dc42359e3323a0f58ce4e0a1e54f56aa
Giorgi Meladze <giorgi.meladze711@gmail.com>
2020-02-18 19:58:32 +0400

    add custom route toggle_status

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   root 'pages#home'
+
   get 'about', to: 'pages#about'
   get 'contact', to:  'pages#contact'
-  resources :blogs
+
+#  get 'blog/:id/toggler', to: 'blogs#toggler', as: "togler-status"
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
